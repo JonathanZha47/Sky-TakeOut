752ac3562d1f246b7ba45eedca8fc367fe995e14
jonathanzha <zhayiwei@bu.edu>
2024-06-13 12:16:57 +0800

    Login feature and Add employer Feature

--- sky-take-out/sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-take-out/sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +25,20 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        if (ex.getMessage().contains("Duplicate entry")) {
+            String[] message = ex.getMessage().split(" ");
+            String name = message[2];
+            String msg = name + MessageConstant.ACCOUNT_ALREADY_EXIST;
+            log.error("重复员工名字: {}", name);
+            return Result.error(msg);
+        } else {
+            log.error("SQL中有未知错误");
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
     }
 
 }
